ce294d1bfed9d78b86b7c6cc927c247a01c21379
muthus <muthusamy.ramalingam@amd.com>
2024-06-24 17:38:16 +0530

    adding logs

--- goamdsmi.go
+++ goamdsmi.go
@@ -48,244 +48,142 @@ package goamdsmi
 */
 import "C"
 
-//GPU ROCM calls
+//GPU ROCM or AMDSMI calls
 func GO_gpu_init() (uint) {
-	if 1 == C.go_shim_amdsmi_present() {
-		return uint(C.go_shim_amdsmigpu_init())
-	}
-	return uint(C.go_shim_rsmi_init())
+	return uint(C.goamdsmi_gpu_init())
 }
 
 func GO_gpu_shutdown() (uint) {
-	if 1 == C.go_shim_amdsmi_present() {
-		return uint(C.go_shim_amdsmigpu_shutdown())
-	}
-	return uint(C.go_shim_rsmi_shutdown())
+	return uint(C.goamdsmi_gpu_shutdown())
 }
 
 func GO_gpu_num_monitor_devices() (uint) {
-	if 1 == C.go_shim_amdsmi_present() {
-		return uint(C.go_shim_amdsmigpu_num_monitor_devices())
-	}
-	return uint(C.go_shim_rsmi_num_monitor_devices())
+	return uint(C.goamdsmi_gpu_num_monitor_devices())
 }
 
 func GO_gpu_dev_name_get(i int) (*C.char) {
-	if 1 == C.go_shim_amdsmi_present() {
-		return C.go_shim_amdsmigpu_dev_name_get(C.uint(i))
-	}
-	return C.go_shim_rsmi_dev_name_get(C.uint(i))
+	return C.goamdsmi_gpu_dev_name_get(C.uint(i))
 }
 
 func GO_gpu_dev_id_get(i int) (C.uint16_t) {
-	if 1 == C.go_shim_amdsmi_present() {
-		return C.uint16_t(C.go_shim_amdsmigpu_dev_id_get(C.uint(i)))
-	}
-	return C.uint16_t(C.go_shim_rsmi_dev_id_get(C.uint(i)))
+	return C.uint16_t(C.goamdsmi_gpu_dev_id_get(C.uint(i)))
 }
 
 func GO_gpu_dev_pci_id_get(i int) (C.uint64_t) {
-	if 1 == C.go_shim_amdsmi_present() {
-		return C.go_shim_amdsmigpu_dev_pci_id_get(C.uint(i))
-	}
-	return C.go_shim_rsmi_dev_pci_id_get(C.uint(i))
+	return C.goamdsmi_gpu_dev_pci_id_get(C.uint(i))
 }
 
 func GO_gpu_dev_vbios_version_get(i int) (*C.char) {
-	if 1 == C.go_shim_amdsmi_present() {
-		return C.go_shim_amdsmigpu_dev_vbios_version_get(C.uint(i))
-	}
-	return C.go_shim_rsmi_dev_vbios_version_get(C.uint(i))
+	return C.goamdsmi_gpu_dev_vbios_version_get(C.uint(i))
 }
 
 func GO_gpu_dev_vendor_name_get(i int) (*C.char) {
-	if 1 == C.go_shim_amdsmi_present() {
-		return C.go_shim_amdsmigpu_dev_vendor_name_get(C.uint(i))
-	}
-	return C.go_shim_rsmi_dev_vendor_name_get(C.uint(i))
+	return C.goamdsmi_gpu_dev_vendor_name_get(C.uint(i))
 }
 
 func GO_gpu_dev_power_cap_get(i int) (C.uint64_t) {
-    if 1 == C.go_shim_amdsmi_present() {
-		return C.go_shim_amdsmigpu_dev_power_cap_get(C.uint(i))
-	}
-	return C.go_shim_rsmi_dev_power_cap_get(C.uint(i))
+    return C.goamdsmi_gpu_dev_power_cap_get(C.uint(i))
 }
 
 func GO_gpu_dev_power_ave_get(i int) (C.uint64_t) {
-    if 1 == C.go_shim_amdsmi_present() {
-		return C.go_shim_amdsmigpu_dev_power_ave_get(C.uint(i))
-	}
-	return C.go_shim_rsmi_dev_power_ave_get(C.uint(i))
+    return C.goamdsmi_gpu_dev_power_ave_get(C.uint(i))
 }
 
 func GO_gpu_dev_temp_metric_get(i int, sensor int, metric int) (C.uint64_t) {
-    if 1 == C.go_shim_amdsmi_present() {
-		return C.go_shim_amdsmigpu_dev_temp_metric_get(C.uint(i), C.uint(sensor), C.uint(metric))
-	}
-	return C.go_shim_rsmi_dev_temp_metric_get(C.uint(i), C.uint(sensor), C.uint(metric))
+    return C.goamdsmi_gpu_dev_temp_metric_get(C.uint(i), C.uint(sensor), C.uint(metric))
 }
 
 func GO_gpu_dev_perf_level_get(i int) (C.uint32_t) {
-	if 1 == C.go_shim_amdsmi_present() {
-		return C.go_shim_amdsmigpu_dev_perf_level_get(C.uint(i))
-	}
-	return C.go_shim_rsmi_dev_perf_level_get(C.uint(i))
+	return C.goamdsmi_gpu_dev_perf_level_get(C.uint(i))
 }
 
 func GO_gpu_dev_overdrive_level_get(i int) (C.uint32_t) {
-	if 1 == C.go_shim_amdsmi_present() {
-		return C.go_shim_amdsmigpu_dev_overdrive_level_get(C.uint(i))
-	}
-	return C.go_shim_rsmi_dev_overdrive_level_get(C.uint(i))
+	return C.goamdsmi_gpu_dev_overdrive_level_get(C.uint(i))
 }
 
 func GO_gpu_dev_mem_overdrive_level_get(i int) (C.uint32_t) {
-	if 1 == C.go_shim_amdsmi_present() {
-		return C.go_shim_amdsmigpu_dev_mem_overdrive_level_get(C.uint(i))
-	}
-	return C.go_shim_rsmi_dev_mem_overdrive_level_get(C.uint(i))
+	return C.goamdsmi_gpu_dev_mem_overdrive_level_get(C.uint(i))
 }
 
 func GO_gpu_dev_gpu_clk_freq_get_sclk(i int) (C.uint64_t) {
-	if 1 == C.go_shim_amdsmi_present() {
-		return C.go_shim_amdsmigpu_dev_gpu_clk_freq_get_sclk(C.uint(i))
-	}
-	return C.go_shim_rsmi_dev_gpu_clk_freq_get_sclk(C.uint(i))
+	return C.goamdsmi_gpu_dev_gpu_clk_freq_get_sclk(C.uint(i))
 }
 
 func GO_gpu_dev_gpu_clk_freq_get_mclk(i int) (C.uint64_t) {
-    if 1 == C.go_shim_amdsmi_present() {
-		return C.go_shim_amdsmigpu_dev_gpu_clk_freq_get_mclk(C.uint(i))
-	}
-	return C.go_shim_rsmi_dev_gpu_clk_freq_get_mclk(C.uint(i))
+    return C.goamdsmi_gpu_dev_gpu_clk_freq_get_mclk(C.uint(i))
 }
 
 func GO_gpu_od_volt_freq_range_min_get_sclk(i int) (C.uint64_t) {
-	if 1 == C.go_shim_amdsmi_present() {
-		return C.go_shim_amdsmigpu_od_volt_freq_range_min_get_sclk(C.uint(i))
-	}
-	return C.go_shim_rsmi_od_volt_freq_range_min_get_sclk(C.uint(i))
+	return C.goamdsmi_gpu_od_volt_freq_range_min_get_sclk(C.uint(i))
 }
 
 func GO_gpu_od_volt_freq_range_min_get_mclk(i int) (C.uint64_t) {
-	if 1 == C.go_shim_amdsmi_present() {
-		return C.go_shim_amdsmigpu_od_volt_freq_range_min_get_mclk(C.uint(i))
-	}
-	return C.go_shim_rsmi_od_volt_freq_range_min_get_mclk(C.uint(i))
+	return C.goamdsmi_gpu_od_volt_freq_range_min_get_mclk(C.uint(i))
 }
 
 func GO_gpu_od_volt_freq_range_max_get_sclk(i int) (C.uint64_t) {
-	if 1 == C.go_shim_amdsmi_present() {
-		return C.go_shim_amdsmigpu_od_volt_freq_range_max_get_sclk(C.uint(i))
-	}
-	return C.go_shim_rsmi_od_volt_freq_range_max_get_sclk(C.uint(i))
+	return C.goamdsmi_gpu_od_volt_freq_range_max_get_sclk(C.uint(i))
 }
 
 func GO_gpu_od_volt_freq_range_max_get_mclk(i int) (C.uint64_t) {
-	if 1 == C.go_shim_amdsmi_present() {
-		return C.go_shim_amdsmigpu_od_volt_freq_range_max_get_mclk(C.uint(i))
-	}
-	return C.go_shim_rsmi_od_volt_freq_range_max_get_mclk(C.uint(i))
+	return C.goamdsmi_gpu_od_volt_freq_range_max_get_mclk(C.uint(i))
 }
 
 func GO_gpu_dev_gpu_busy_percent_get(i int) (C.uint64_t) {
-	if 1 == C.go_shim_amdsmi_present() {
-		return C.go_shim_amdsmigpu_dev_gpu_busy_percent_get(C.uint(i))
-	}
-	return C.go_shim_rsmi_dev_gpu_busy_percent_get(C.uint(i))
+	return C.goamdsmi_gpu_dev_gpu_busy_percent_get(C.uint(i))
 }
 
 func GO_gpu_dev_gpu_memory_busy_percent_get(i int) (C.uint64_t) {
-	if 1 == C.go_shim_amdsmi_present() {
-		return C.go_shim_amdsmigpu_dev_gpu_memory_busy_percent_get(C.uint(i))
-	}
-	return C.go_shim_rsmi_dev_gpu_memory_busy_percent_get(C.uint(i))
+	return C.goamdsmi_gpu_dev_gpu_memory_busy_percent_get(C.uint(i))
 }
 
 func GO_gpu_dev_gpu_memory_usage_get (i int) (C.uint64_t) {
-	if 1 == C.go_shim_amdsmi_present() {
-		return C.go_shim_amdsmigpu_dev_gpu_memory_usage_get(C.uint(i))
-	}
-	return C.go_shim_rsmi_dev_gpu_memory_usage_get(C.uint(i))
+	return C.goamdsmi_gpu_dev_gpu_memory_usage_get(C.uint(i))
 }
 
 func GO_gpu_dev_gpu_memory_total_get (i int) (C.uint64_t) {
-	if 1 == C.go_shim_amdsmi_present() {
-		return C.go_shim_amdsmigpu_dev_gpu_memory_total_get(C.uint(i))
-	}
-	return C.go_shim_rsmi_dev_gpu_memory_total_get(C.uint(i))
+	return C.goamdsmi_gpu_dev_gpu_memory_total_get(C.uint(i))
 }
 
-//CPU ESMI calls
+//CPU ESMI or AMDSMI calls
 func GO_cpu_init() (uint) {
-	if 1 == C.go_shim_amdsmi_present() {
-		return uint(C.go_shim_amdsmicpu_init())
-	}
-	return uint(C.go_shim_esmi_init())
+	return uint(C.goamdsmi_cpu_init())
 }
 
 func GO_cpu_number_of_sockets_get() (uint) {
-	if 1 == C.go_shim_amdsmi_present() {
-		return uint(C.go_shim_amdsmicpu_number_of_sockets_get())
-	}
-	return uint(C.go_shim_esmi_number_of_sockets_get())
+	return uint(C.goamdsmi_cpu_number_of_sockets_get())
 }
 
 func GO_cpu_number_of_threads_get() (uint) {
-	if 1 == C.go_shim_amdsmi_present() {
-		return uint(C.go_shim_amdsmicpu_number_of_threads_get())
-	}
-	return uint(C.go_shim_esmi_number_of_threads_get())
+	return uint(C.goamdsmi_cpu_number_of_threads_get())
 }
 
 func GO_cpu_threads_per_core_get() (uint) {
-	if 1 == C.go_shim_amdsmi_present() {
-		return uint(C.go_shim_amdsmicpu_threads_per_core_get())
-	}
-	return uint(C.go_shim_esmi_threads_per_core_get())
+	return uint(C.goamdsmi_cpu_threads_per_core_get())
 }
 
 func GO_cpu_core_energy_get(i int) (C.uint64_t) {
-	if 1 == C.go_shim_amdsmi_present() {
-		return C.go_shim_amdsmicpu_core_energy_get(C.uint(i))
-	}
-	return C.go_shim_esmi_core_energy_get(C.uint(i))
+	return C.goamdsmi_cpu_core_energy_get(C.uint(i))
 }
 
 func GO_cpu_core_boostlimit_get(i int) (C.uint32_t) {
-	if 1 == C.go_shim_amdsmi_present() {
-		return C.go_shim_amdsmicpu_core_boostlimit_get(C.uint(i))
-	}
-	return C.go_shim_esmi_core_boostlimit_get(C.uint(i))
+	return C.goamdsmi_cpu_core_boostlimit_get(C.uint(i))
 }
 
 func GO_cpu_socket_energy_get(i int) (C.uint64_t) {
-	if 1 == C.go_shim_amdsmi_present() {
-		return C.go_shim_amdsmicpu_socket_energy_get(C.uint(i))
-	}
-	return C.go_shim_esmi_socket_energy_get(C.uint(i))
+	return C.goamdsmi_cpu_socket_energy_get(C.uint(i))
 }
 
 func GO_cpu_socket_power_get(i int) (C.uint32_t) {
-	if 1 == C.go_shim_amdsmi_present() {
-		return C.go_shim_amdsmicpu_socket_power_get(C.uint(i))
-	}
-	return C.go_shim_esmi_socket_power_get(C.uint(i))
+	return C.goamdsmi_cpu_socket_power_get(C.uint(i))
 }
 
 func GO_cpu_socket_power_cap_get(i int) (C.uint32_t) {
-	if 1 == C.go_shim_amdsmi_present() {
-		return C.go_shim_amdsmicpu_socket_power_cap_get(C.uint(i))
-	}
-	return C.go_shim_esmi_socket_power_cap_get(C.uint(i))
+	return C.goamdsmi_cpu_socket_power_cap_get(C.uint(i))
 }
 
 func GO_cpu_prochot_status_get(i int) (C.uint32_t) {
-	if 1 == C.go_shim_amdsmi_present() {
-		return C.go_shim_amdsmicpu_prochot_status_get(C.uint(i))
-	}
-	return C.go_shim_esmi_prochot_status_get(C.uint(i))
+	return C.goamdsmi_cpu_prochot_status_get(C.uint(i))
 }
 
 
